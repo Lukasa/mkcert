0bcdd739c14f6b9cda1efa5e293cec70c00d9d51
Cory Benfield <lukasaoz@gmail.com>
2014-09-04 20:28:53 +0100

    Begin to define blacklist API.

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"runtime"
+	"strings"
 )
 
 const CERT_URL = "https://hg.mozilla.org/mozilla-central/raw-file/tip/security/nss/lib/ckfw/builtins/certdata.txt"
@@ -24,8 +25,27 @@ func updateCertificates() {
 	certificates = certs.OutputTrustedCerts(objects)
 }
 
-func serveCertificates(w http.ResponseWriter, r *http.Request) {
+// Parses the exceptions from the path.
+func getExceptions(path string, prefix string) map[string]interface{} {
+	// Remove the prefix.
+	query := string(path[len(prefix):])
+
+	// Split the query on each '+' character.
+	components := strings.Split(query, "+")
+
 	exceptions := make(map[string]interface{})
+	for _, component := range components {
+		exceptions[component] = nil
+	}
+
+	return exceptions
+}
+
+// serveBlacklistCertificates serves certificates using a blacklist. The
+// expected form of the URL is: /generate/name1+name2+name3, where name1 and
+// friends are the labels to exclude from the list.
+func serveBlacklistCertificates(w http.ResponseWriter, r *http.Request) {
+	exceptions := getExceptions(r.URL.Path, "/generate/")
 	certs.WriteCerts(w, certificates, false, exceptions)
 }
 
@@ -37,6 +57,6 @@ func main() {
 	updateCertificates()
 
 	// Start the HTTP server.
-	http.HandleFunc("/", serveCertificates)
+	http.HandleFunc("/generate/", serveBlacklistCertificates)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
